c1fbd5042278f7e6f1cd1738e448b2226256b2cb
Florianschmidtwelzow <florian.schmidt.welzow@t-online.de>
2015-04-25 16:14:47 +0200

    Show ads only for anonymous users
    Change-Id: Ie4c4aea3ec2d45dd72b7be12c5a8829883bd3f1f

--- DroidWiki.hooks.php
+++ DroidWiki.hooks.php
@@ -9,7 +9,7 @@ class DroidWikiHooks {
 	public static function onSkinTemplateOutputPageBeforeExec(
 		SkinTemplate &$sk, QuickTemplate &$tpl
 	) {
-		if ( $sk->getSkinName() === 'vector' ) {
+		if ( $sk->getSkinName() === 'vector' && self::checkShowAd( false, $sk, $tpl ) ) {
 			$out = $sk->getOutput();
 			$adContent = Html::openElement(
 					'aside',
@@ -44,6 +44,47 @@ class DroidWikiHooks {
 			$out->mBodytext = $adContent . $out->mBodytext;
 		}
 		return true;
+	}
+
+	/**
+	 * Checks whether it's allowed to show advertising banners on this page.
+	 * The check includes the actual login state of the user and the actual site requested.
+	 * You can disallow the display of ads on specific pages using the $wgNoAdSites array in
+	 * LocalSettings.php
+	 * @param boolean $showtologin Is the ad block, the check is requested from, only visible
+	 * for logged in users, set this to true, otherwise this will be false and the function gives
+	 * only true, if the user is logged out.
+	 * @return boolean
+	 */
+	private static function checkShowAd( $showtologin = false, SkinTemplate $sk, BaseTemplate $tpl ) {
+		global $wgNoAdSites;
+		$loginshow = false;
+		if ( $showtologin && $tpl->data['loggedin'] ) {
+			$loginshow = true;
+			if ( !empty( $tpl->data['showadlogin'] ) ) {
+				return $tpl->data['showadlogin'];
+			}
+		}
+		if ( !$showtologin && !$tpl->data['loggedin'] ) {
+			$loginshow = true;
+			if ( !empty( $tpl->data['showad'] ) ) {
+				return $tpl->data['showad'];
+			}
+		}
+		// get the URL title (don't use title object, the configuration $wgNoAdSites
+		// in LocalSettings specifies url title
+		$urltitle = $sk->getRequest()->getText( 'title' );
+		if ( !in_array( $urltitle, $wgNoAdSites ) && $loginshow ) {
+			if ( $showtologin && $tpl->data['loggedin'] ) {
+				$tpl->data['showadlogin'] = true;
+			}
+			if ( !$showtologin && !$tpl->data['loggedin'] ) {
+				$tpl->data['showad'] = true;
+			}
+			return true;
+		}
+
+		return false;
 	}
 
 	/**
