96eb02ab2fa53c5cb64a2cfbd61ec4dc70270913
Florian <florian.schmidt.welzow@t-online.de>
2015-12-18 22:53:11 +0100

    Fix Stackoverflow error
    Change-Id: Id8283abfa700844c8e3abc453df4b9cc575b9737

--- DroidWiki.hooks.php
+++ DroidWiki.hooks.php
@@ -214,7 +214,7 @@ class DroidWikiHooks {
 	public static function onPageContentLanguage( Title $title, Language &$pageLang, $userLang  ) {
 		// FIXME: temporary hack for T121666, this shouldn't be needed
 		if ( strpos( $title->getText(), 'Android Training/' ) != -1 ) {
-			$pageLang = $title->getPageLanguage();
+			$pageLang = wfGetLangObj( 'en' );
 		}
 	}
 }
