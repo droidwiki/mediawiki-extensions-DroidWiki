49a983dc8ac4010bf52ddc4724f562a808fde10e
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2018-07-08 02:01:55 +0200

    Refactor code and remove some duplications

--- DroidWiki.hooks.php
+++ DroidWiki.hooks.php
@@ -1,105 +1,64 @@
 <?php
+
 class DroidWikiHooks {
 	private static $adAlreadyAdded = false;
+
+	const ADSENSE_AD_CLIENT = 'ca-pub-4622825295514928';
+
+	const ADSENSE_AD_PUSH_CODE = '<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
 
 	public static function onSkinTemplateOutputPageBeforeExec(
 		SkinTemplate &$sk, QuickTemplate &$tpl
 	) {
 		if (
-			!self::$adAlreadyAdded &&
-			$sk->getSkinName() === 'vector' &&
+			!self::$adAlreadyAdded && $sk->getSkinName() === 'vector' &&
 			self::checkShowAd( $sk, 'right' )
 		) {
-			$out = $sk->getOutput();
 			self::$adAlreadyAdded = true;
-			$adContent = Html::openElement(
-					'aside',
-					array(
-						'id' => 'adContent',
-						'class' => 'mw-body-rightcontainer',
-					)
-				) .
-				Html::element(
-					'script',
-					array(
-						'async',
-						'src' => '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-					)
-				) .
-				Html::openElement(
-					'ins',
-					array(
-						'class' => 'adsbygoogle',
-						'style' => 'display:inline-block;width:160px;height:600px',
-						'data-ad-client' => 'ca-pub-4622825295514928',
-						'data-ad-slot' => '8031689899',
-					)
-				) .
-				Html::closeElement( 'ins' ) .
-				Html::openElement(
-					'script'
-				) .
-				'(adsbygoogle = window.adsbygoogle || []).push({});' .
-				Html::closeElement( 'script' ) .
-				Html::closeElement( 'aside' );
-
-			$tpl->data['bodytext'] = $adContent . $tpl->data['bodytext'];
+			self::addAdCodeToBodyText( $tpl );
 		}
 
 		$lockedPages = array(
-			SpecialPage::getTitleFor( 'MobileDiff' )->getRootText()
+			SpecialPage::getTitleFor( 'MobileDiff' )->getRootText(),
 		);
 		// this is the mobile web ad block
 		if (
 			ExtensionRegistry::getInstance()->isLoaded( 'MobileFrontend' ) &&
 			MobileContext::singleton()->shouldDisplayMobileView() &&
-			!in_array( $sk->getTitle()->getRootText(), $lockedPages )
+		    !in_array( $sk->getTitle()->getRootText(), $lockedPages )
 		) {
-			$tpl->data['bodytext'] = $tpl->data['bodytext'] .
+			$tpl->data['bodytext'] =
+				$tpl->data['bodytext'] .
 				Html::openElement( 'div', [ 'id' => 'ad-cat', 'class' => 'adsbygoogleCategory' ] ) .
-				Html::element(
-					'script',
-					array(
-						'async',
-						'src' => '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-					)
-				) .
-				Html::openElement(
-					'ins',
-					array(
-						'class' => 'adsbygoogle',
-						'style' => 'display:block',
-						'data-ad-client' => 'ca-pub-4622825295514928',
-						'data-ad-slot' => '6645983899',
-						'data-ad-format' => 'horizontal',
-					)
-				) .
-				Html::closeElement( 'ins' ) .
-				Html::openElement(
-					'script'
-				) .
-				'(adsbygoogle = window.adsbygoogle || []).push({});' .
-				Html::closeElement( 'script' ) .
-				Html::closeElement( 'div' );
+				self::getAdSenseScriptTag() .
+				self::getAdSenseINSBlock( '6645983899', 'horizontal', 'display:block' ) .
+				self::ADSENSE_AD_PUSH_CODE . Html::closeElement( 'div' );
 		}
 
 
-		$devDestination = Skin::makeInternalOrExternalUrl( $sk->msg( 'droidwiki-developers-url' )->inContentLanguage()->text() );
+		$devDestination =
+			Skin::makeInternalOrExternalUrl( $sk->msg( 'droidwiki-developers-url' )
+				->inContentLanguage()
+				->text() );
 		$devLink = Html::element(
 			'a',
-			array( 'href' => $devDestination ),
+			[ 'href' => $devDestination ],
 			$sk->msg( 'droidwiki-developers' )->text()
 		);
 		$tpl->set( 'developers', $devLink );
 		$tpl->data['footerlinks']['places'][] = 'developers';
-		$cookieDestination = Skin::makeInternalOrExternalUrl( $sk->msg( 'droidwiki-imprint-url' )->inContentLanguage()->text() );
+		$cookieDestination =
+			Skin::makeInternalOrExternalUrl( $sk->msg( 'droidwiki-imprint-url' )
+				->inContentLanguage()
+				->text() );
 		$cookieLink = Html::element(
 			'a',
-			array( 'href' => $cookieDestination ),
+			[ 'href' => $cookieDestination ],
 			$sk->msg( 'droidwiki-imprint' )->text()
 		);
 		$tpl->set( 'imprint', $cookieLink );
 		$tpl->data['footerlinks']['places'][] = 'imprint';
+
 		return true;
 	}
 
@@ -109,36 +68,13 @@ class DroidWikiHooks {
 		}
 
 		// the desktop ad block is slightly different
-		$data = Html::openElement(
-				'div',
-				array(
-					'class' => 'adsbygoogleCategory'
-				)
-			) .
-		        Html::element(
-			        'script',
-			        array(
-				        'async',
-				        'src' => '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-			        )
-		        ) .
-		        Html::openElement(
-			        'ins',
-			        array(
-				        'class' => 'adsbygoogle',
-				        'style' => 'display:block',
-				        'data-ad-client' => 'ca-pub-4622825295514928',
-				        'data-ad-slot' => '6216454699',
-				        'data-ad-format' => 'auto',
-			        )
-		        ) .
-		        Html::closeElement( 'ins' ) .
-		        Html::openElement(
-			        'script'
-		        ) .
-		        '(adsbygoogle = window.adsbygoogle || []).push({});' .
-		        Html::closeElement( 'script' ) .
-		        Html::closeElement( 'div' );
+		$data = Html::openElement( 'div', array(
+				'class' => 'adsbygoogleCategory',
+			) ) .
+		    self::getAdSenseScriptTag() .
+		    self::getAdSenseINSBlock( '6216454699', 'auto', 'display:block' ) .
+		    self::ADSENSE_AD_PUSH_CODE .
+		    Html::closeElement( 'div' );
 	}
 
 	public static function checkShowAd( SkinTemplate $sk, $position = 'right' ) {
@@ -175,23 +111,19 @@ class DroidWikiHooks {
 	}
 
 	public static function onBeforePageDisplay( OutputPage $out, Skin $sk ) {
-		$modules = array(
-			'ext.DroidWiki.adstyle.category'
+		$skinModules = array(
+			'ext.DroidWiki.adstyle.category',
 		);
 		if ( $sk->getSkinName() === 'vector' && self::checkShowAd( $sk ) ) {
-			$modules[] = 'ext.DroidWiki.adstyle';
+			$skinModules[] = 'ext.DroidWiki.adstyle';
 		}
-		$out->addModules( $modules );
-
-		// main page adjustments
 		if ( $out->getTitle()->isMainPage() ) {
-			$out->addModuleStyles( 'ext.DroidWiki.mainpage.styles' );
+			$skinModules[] = 'ext.DroidWiki.mainpage.styles';
 		}
+		$out->addModuleStyles( $skinModules );
 
-		$out->addHeadItem( 'google_ad_sitelevel',
-			'<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>' );
-		$out->addHeadItem( 'google_ad_sitelevel_config',
-		'<script>
+		$out->addHeadItem( 'google_ad_sitelevel', self::getAdSenseScriptTag() );
+		$out->addHeadItem( 'google_ad_sitelevel_config', '<script>
 		(adsbygoogle = window.adsbygoogle || []).push({
 			google_ad_client: "ca-pub-4622825295514928",
 			enable_page_level_ads: true
@@ -205,17 +137,15 @@ class DroidWikiHooks {
 		$gitInfo = new GitInfo( "$IP/../mw-config/mw-config" );
 		if ( $gitInfo ) {
 			$software['[http://git.go2tech.de/?p=droidwiki%2Foperations%2Fmediawiki-config.git MWC]'] =
-				'[' .
-				$gitInfo->getHeadViewUrl() .
-				' ' .
-				substr( $gitInfo->getHeadSHA1(), 0, 7 ) . ']';
+				'[' . $gitInfo->getHeadViewUrl() . ' ' . substr( $gitInfo->getHeadSHA1(), 0, 7 ) .
+				']';
 		}
 	}
 
 	public static function onSkinCopyrightFooter( $title, $type, &$msg, &$link ) {
 		global $wgRightsUrl;
 
-		if( strpos( $wgRightsUrl, 'creativecommons.org/licenses/by-sa/3.0' ) !== false ) {
+		if ( strpos( $wgRightsUrl, 'creativecommons.org/licenses/by-sa/3.0' ) !== false ) {
 			if ( $type !== 'history' ) {
 				$msg = 'droidwiki-copyright';
 			}
@@ -224,7 +154,7 @@ class DroidWikiHooks {
 		return true;
 	}
 
-	public static function onPageContentLanguage( Title $title, Language &$pageLang, $userLang  ) {
+	public static function onPageContentLanguage( Title $title, Language &$pageLang, $userLang ) {
 		// FIXME: temporary hack for T121666, this shouldn't be needed
 		if ( strpos( $title->getText(), 'Android Training/' ) !== false ) {
 			$pageLang = wfGetLangObj( 'en' );
@@ -236,11 +166,52 @@ class DroidWikiHooks {
 	 * which should indicate to the ContentTranslation extension, that the main droidwiki language (german, with interwiki link
 	 * de but mapped to www for ContentTranslation) is already translated.
 	 */
-	public static function onSkinTemplateGetLanguageLink( &$languageLink, $languageLinkTitle, Title $title, OutputPage $out ) {
-		if ( strpos( $languageLink['class'],  'interwiki-de' ) === -1 ) {
+	public static function onSkinTemplateGetLanguageLink(
+		&$languageLink, $languageLinkTitle, Title $title, OutputPage $out
+	) {
+		if ( strpos( $languageLink['class'], 'interwiki-de' ) === - 1 ) {
 			return;
 		}
 
 		$languageLink['class'] .= ' interwiki-www';
+	}
+
+	private static function addAdCodeToBodyText( QuickTemplate &$tpl ) {
+		$adContent = Html::openElement( 'aside', [
+				'id' => 'adContent',
+				'class' => 'mw-body-rightcontainer',
+			] ) .
+		    self::getAdSenseScriptTag() .
+		    self::getAdSenseINSBlock(
+		    	'8031689899',
+			    null,
+			    'display:inline-block;width:160px;height:600px'
+		    ) .
+		    self::ADSENSE_AD_PUSH_CODE .
+		    Html::closeElement( 'aside' );
+
+		$tpl->data['bodytext'] = $adContent . $tpl->data['bodytext'];
+	}
+
+	private static function getAdSenseScriptTag() {
+		return Html::element( 'script', [
+			'async',
+			'src' => '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+		] );
+	}
+
+	private static function getAdSenseINSBlock( $slot, $adFormat = null, $style = '' ) {
+		$attribs = [
+			'class' => 'adsbygoogle',
+			'style' => $style,
+			'data-ad-client' => self::ADSENSE_AD_CLIENT,
+			'data-ad-slot' => $slot,
+		];
+
+		if ( $adFormat ) {
+			$attribs['data-ad-format'] = $adFormat;
+		}
+
+		return Html::element( 'ins', $attribs );
 	}
 }
